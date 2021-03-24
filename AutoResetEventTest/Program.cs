a2bc465d66cddda76df4a8c1d8cb3e928723e69d
StevenGu <SGU03@beckman.com>
2021-03-24 11:23:11 +0800

    11

---
+++ AutoResetEventTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoResetEventTest
+{
+    class Program
+    {
+        private static AutoResetEvent event_1 = new AutoResetEvent(initialState: false);
+
+        private static void Run()
+        {
+             AutoResetEvent event_2 = new AutoResetEvent(initialState: false);
+          Console.WriteLine("State is be set;");
+        }
+        static void Main(string[] args)
+        {
+            Run();
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
